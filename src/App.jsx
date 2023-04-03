dd73099557d67ef2da78c13b45eae076adfec816
Puneeth-hub <puneethkumargajjela@gmail.com>
2023-04-03 17:27:27 +0530

    boxigo-progress

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react"; 
+import axios from "axios";
+function App(){
+  const [dataList, setDataList] = useState("")
+  
+  const apiCall = async()=>{
+    const apiUrl = 'http://test.api.boxigo.in/sample-data/' 
+    const options = {
+      method:'GET'
+    }
+    const response = await fetch(apiUrl, options) 
+    const data = await response.json() 
+    setDataList(data)
+    
+  }
+  
+  
+ return(
+  <div>
+    <button onClick={()=> apiCall()}>click</button>
+    <>
+    {dataList && dataList.Customer_Estimate_Flow.map((each)=>{
+      return ( <><p>{each.user_id}</p> 
+         <p>{each.distance}</p>
+      </>)
+})}
+    </>
+   
+  </div>
+ )
+
+}
+
+export default App;
